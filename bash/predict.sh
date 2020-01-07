d3743f8cad176435cf70edeec24b3b4a0b44db5c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-01-07 15:21:08 +0100

    Slurm array for 3 experiments

--- bash/predict.sh
+++ bash/predict.sh
@@ -2,32 +2,31 @@
 #SBATCH --mem=30G
 #SBATCH --time=0-1
 #SBATCH -p gpu --gres=gpu:titanx:1
-
-# examples of predicting commands
+#SBATCH --array=1-3
 
 # UCCA
 CUDA_VISIBLE_DEVICES=0 \
 allennlp predict \
 --cuda-device 0 \
---output-file ucca-output.dev.mrp \
+--output-file ucca-output$SLURM_ARRAY_TASK_ID.dev.mrp \
 --predictor transition_predictor_ucca \
 --include-package utils \
 --include-package modules \
 --use-dataset-reader \
 --batch-size 32 \
 --silent \
-checkpoints/ucca_bert \
+checkpoints/ucca_bert$SLURM_ARRAY_TASK_ID \
 data/ewt.dev.aug.mrp
 
 CUDA_VISIBLE_DEVICES=0 \
 allennlp predict \
 --cuda-device 0 \
---output-file ucca-output.test.mrp \
+--output-file ucca-output$SLURM_ARRAY_TASK_ID.test.mrp \
 --predictor transition_predictor_ucca \
 --include-package utils \
 --include-package modules \
 --use-dataset-reader \
 --batch-size 32 \
 --silent \
-checkpoints/ucca_bert \
+checkpoints/ucca_bert$SLURM_ARRAY_TASK_ID \
 data/ewt.test.aug.mrp
--- bash/train.sh
+++ bash/train.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --mem=30G
-#SBATCH --time=1-0
+#SBATCH --time=5-0
 #SBATCH -p gpu --gres=gpu:titanx:1
-
-# examples of training commands
+#SBATCH --array=1-3%1
 
 # UCCA
 CUDA_VISIBLE_DEVICES=0 \
@@ -12,10 +11,10 @@ DEV_PATH=data/ewt.dev.aug.mrp \
 BERT_PATH=bert/wwm_cased_L-24_H-1024_A-16 \
 WORD_DIM=1024 \
 LOWER_CASE=FALSE \
-BATCH_SIZE=4 \
+BATCH_SIZE=1 \
 allennlp train \
--s checkpoints/ucca_bert \
+-s checkpoints/ucca_bert$SLURM_ARRAY_TASK_ID \
 --include-package utils \
 --include-package modules \
 --file-friendly-logging \
-config/transition_bert_ucca.jsonnet
+config/transition_bert_ucca.jsonnet $*
