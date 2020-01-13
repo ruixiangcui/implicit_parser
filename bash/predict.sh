77320001a6da1c6a87b592cbdedd0ea8639f5e6a
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-01-13 16:35:20 +0100

    Convert MRP to XML after prediction

--- bash/predict.sh
+++ bash/predict.sh
@@ -4,26 +4,20 @@
 #SBATCH --array=1-3
 
 # UCCA
-allennlp predict \
---cuda-device -1 \
---output-file ucca-output$SLURM_ARRAY_TASK_ID.dev.mrp \
---predictor transition_predictor_ucca \
---include-package utils \
---include-package modules \
---use-dataset-reader \
---batch-size 32 \
---silent \
-checkpoints/ucca_bert$SLURM_ARRAY_TASK_ID \
-data/ewt.dev.aug.mrp
+for split in dev test; do
+  allennlp predict \
+  --cuda-device -1 \
+  --output-file ucca-output$SLURM_ARRAY_TASK_ID.$split.mrp \
+  --predictor transition_predictor_ucca \
+  --include-package utils \
+  --include-package modules \
+  --use-dataset-reader \
+  --batch-size 32 \
+  --silent \
+  checkpoints/ucca_bert$SLURM_ARRAY_TASK_ID \
+  data/ewt.$split.aug.mrp
 
-allennlp predict \
---cuda-device -1 \
---output-file ucca-output$SLURM_ARRAY_TASK_ID.test.mrp \
---predictor transition_predictor_ucca \
---include-package utils \
---include-package modules \
---use-dataset-reader \
---batch-size 32 \
---silent \
-checkpoints/ucca_bert$SLURM_ARRAY_TASK_ID \
-data/ewt.test.aug.mrp
+  mkdir -p data/ucca-output$SLURM_ARRAY_TASK_ID.$split
+  python toolkit/mtool/main.py data/ucca-output$SLURM_ARRAY_TASK_ID.$split.mrp data/ucca-output$SLURM_ARRAY_TASK_ID.$split.xml --read mrp --write ucca
+  csplit -zk data/ucca-output$SLURM_ARRAY_TASK_ID.$split.xml '/^<root/' -f '' -b "data/ucca-output$SLURM_ARRAY_TASK_ID.$split/%03d.xml" {553}
+done
