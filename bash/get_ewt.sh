0e221cbc6eadebb19c789aef4b7f23f16dafd42c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-05-12 16:08:10 +0200

    Script to get gold UCCA EWT

--- bash/get_ewt.sh
+++ bash/get_ewt.sh
@@ -6,3 +6,10 @@ python toolkit/augment_data.py data/ewt.conllu ../mrp/2019/training/ucca/ewt.mrp
 for split in train dev test; do
   grep -Ff file-lists/$split.txt data/ewt.aug.mrp > data/ewt.$split.aug.mrp
 done
+
+# Get gold data to evaluate on
+git clone https://github.com/UniversalConceptualCognitiveAnnotation/UCCA_English-EWT -b v1.0-sentences data/ewt
+mkdir data/ewt/{train,dev,test}
+for split in train dev test; do
+  xargs -I % find data/ewt -maxdepth 1 -name '%*.pickle' < file-lists/$split.txt | xargs ln -srt data/ewt/$split
+done
