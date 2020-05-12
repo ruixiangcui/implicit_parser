370c8bbd1acfb91010eeec52c7a3529d3011a53c
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2020-05-12 15:46:47 +0200

    Remove unneeded files, for frameworks other than UCCA

--- toolkit/tamr_aligner/smatch/setup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize(Extension("_smatch", sources=["_smatch.pyx", "_gain.cc"], language="c++")))
